33a423d8a57fa7513b7e23c8d014a2d2b7a411e3
Yasunari Watanabe <yasunariw@users.noreply.github.com>
2018-10-29 12:20:45 -0400

    Add ast.mli file.

---
+++ ast.mli
@@ -0,0 +1,8 @@
+type program = string list * string list
+
+(* type stmt =
+  For of expr * expr * expr * stmt
+  ForNode of string * string * stmt
+  ForEdge of string * string * stmt
+  While of expr * stmt
+  *)
