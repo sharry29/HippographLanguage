176798e19eb48e6090c816339d4e7cd2d4b7b18d
Harry Smith <harry@dyn-160-39-161-194.dyn.columbia.edu>
2018-11-07 17:49:28 -0500

    basic toplevel added, skipping sast

--- hippograph.ml
+++ hippograph.ml
@@ -1,4 +1,7 @@
 let _ =
     let lexbuf = Lexing.from_channel stdin in
-     let result = Parser.program Scanner.token lexbuf in
-        print_string "Hello, World!\n"; flush stdout
+    let ast = Parser.program Scanner.token lexbuf in
+    let sast = Semant.check ast in
+    let m = Codegen.translate sast in
+   	Llvm_analysis.assert_valid_module m;
+	print_string (Llvm.string_of_llmodule m)
