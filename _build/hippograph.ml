0c95bbc70f238eefd3e5c57d3ccc321fedf7a79d
Yasunari Watanabe <yasunariw@users.noreply.github.com>
2018-11-14 15:26:35 -0500

    Stop tracking _build directory and compiled binaries.

--- _build/hippograph.ml
+++
@@ -1,9 +0,0 @@
-let _ =
-let lexbuf = Lexing.from_channel stdin in
-    let ast = Parser.program Scanner.token lexbuf in
-    print_string (Ast.string_of_program ast);
-    let sast = Semant.check ast in
-    print_string (Sast.string_of_sprogram sast);
-	let m = Codegen.translate sast in
-    Llvm_analysis.assert_valid_module m;
-    print_string (Llvm.string_of_llmodule m)
