7918fc4ade676abb40ac962f7433c4a346138630
Yasunari Watanabe <yasunariw@users.noreply.github.com>
2018-11-14 16:30:41 -0500

    Make the binary run a file directly without needing to pipe it in.

--- hippograph.ml
+++ hippograph.ml
@@ -1,9 +1,24 @@
+
+type action = Ast | LLVM_IR | Compile
+
 let _ =
-let lexbuf = Lexing.from_channel stdin in
-    let ast = Parser.program Scanner.token lexbuf in
-(*     print_string (Ast.string_of_program ast); *)
-    let sast = Semant.check ast in
-(*     print_string (Sast.string_of_sprogram sast); *)
-	let m = Codegen.translate sast in
+  let action = ref Compile in
+  let set_action a () = action := a in
+  let speclist = [
+    ("-a", Arg.Unit (set_action Ast), "Print the SAST");
+    ("-l", Arg.Unit (set_action LLVM_IR), "Print the generated LLVM IR");
+    ("-c", Arg.Unit (set_action Compile),
+      "Check and print the generated LLVM IR (default)");
+  ] in  
+  let usage_msg = "usage: ./microc.native [-a|-l|-c] [file.mc]" in
+  let channel = ref stdin in
+  Arg.parse speclist (fun filename -> channel := open_in filename) usage_msg;
+  let lexbuf = Lexing.from_channel !channel in
+  let ast = Parser.program Scanner.token lexbuf in
+  let sast = Semant.check ast in
+  match !action with
+    Ast -> print_string (Sast.string_of_sprogram sast)
+  | LLVM_IR -> print_string (Llvm.string_of_llmodule (Codegen.translate sast))
+  | Compile -> let m = Codegen.translate sast in
     Llvm_analysis.assert_valid_module m;
     print_string (Llvm.string_of_llmodule m)
--- testall.sh
+++ testall.sh
@@ -92,7 +92,7 @@ Check() {
     generatedfiles=""
 
     generatedfiles="$generatedfiles ${basename}.ll ${basename}.s ${basename}.exe ${basename}.out" &&
-    Run "$HIPPOGRAPH" "<" "$1" ">" "${basename}.ll" &&
+    Run "$HIPPOGRAPH" "$1" ">" "${basename}.ll" &&
     Run "$LLC" "-relocation-model=pic" "${basename}.ll" ">" "${basename}.s" &&
     Run "$CC" "-o" "${basename}.exe" "${basename}.s" &&
     Run "./${basename}.exe" > "${basename}.out" &&
@@ -164,13 +164,6 @@ LLIFail() {
 }
 
 which "$LLI" >> $globallog || LLIFail
-
-# if [ ! -f printbig.o ]
-# then
-#     echo "Could not find printbig.o"
-#     echo "Try \"make printbig.o\""
-#     exit 1
-# fi
 
 if [ $# -ge 1 ]
 then
