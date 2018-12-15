d51c88ff6543b2fce020d11b5b90d099f64cb5f1
Yasunari Watanabe <yasunariw@users.noreply.github.com>
2018-12-15 16:30:48 -0500

    Fix function type of graph_set_edge_str_int.

--- codegen.ml
+++ codegen.ml
@@ -90,7 +90,7 @@ let translate (globals, functions) =
   let graph_set_edge_int_int_t : L.lltype = L.var_arg_function_type i32_t [| void_ptr_t; i32_t; i32_t; i32_t |] in
   let graph_set_edge_int_int_func : L.llvalue = L.declare_function "graph_set_edge_int_int" graph_set_edge_int_int_t the_module in
 
-  let graph_set_edge_str_int_t : L.lltype = L.var_arg_function_type i32_t [| void_ptr_t; i32_t; i32_t; i32_t |] in
+  let graph_set_edge_str_int_t : L.lltype = L.var_arg_function_type i32_t [| void_ptr_t; str_t; str_t; i32_t |] in
   let graph_set_edge_str_int_func : L.llvalue = L.declare_function "graph_set_edge_str_int" graph_set_edge_str_int_t the_module in
 
   let print_node_t : L.lltype = L.var_arg_function_type void_t [| void_ptr_t |] in
